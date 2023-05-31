8937f3e7fb53e33aeedc411f46142c09bf46565a
Dave <davidmendoza911119@gmail.com>
2023-05-30 23:35:22 -0600

    adding gitignore

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 
+//setting endpoints
 app.use("/api/v1/post", postRoutes)
 app.use("/api/v1/dalle", dalleRoutes)
 
